c5f996ebd8e847ffb9058d7d7070f8c7542d63b9
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-12 21:27:49 +0200

    add results

---
+++ getting-started/index.js
@@ -0,0 +1,80 @@
+// 1
+
+var firstName;
+firstName = "Nadia";
+var lastName = "Daruiz"
+console.log (firstName, lastName);
+
+// 2 
+
+var name = "John Smith";
+var city = "Berlin";
+var age = "43 years old";
+var profession = "Teacher";
+var hobby = "Singing";
+console.log (name);
+console.log (age);
+console.log (city);
+console.log (profession);
+console.log (hobby);
+
+// 3
+
+console.log (name + " " + "is a " + age + " " + profession + " " + "who lives in " + city);
+
+// 4
+
+//let length = 16.78; --> number
+//let lastName = "Johnson"; --> string
+//let car = ""; --> string
+//let isOpen = true; --> boolean
+
+console.log (typeof 16.78);
+console.log (typeof "Johnson");
+console.log (typeof "");
+console.log (typeof true);
+
+// 5 
+
+var ageMark = 20+13;
+var ageJohn = "35";
+console.log (ageMark, ageJohn);
+
+// 6
+
+console.log (ageJohn > ageMark)
+
+// 7 
+
+// 8
+
+var programmingLanguage = "JavaScript";
+var isFun = true;
+console.log (programmingLanguage + " is fun: " + isFun);
+
+// 9 
+
+var test; 
+console.log (typeof "test");
+
+// 10
+
+var calculation;
+calculation = 1+1;
+console.log (calculation);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
