e88756f3688093f0a7007d65b9bd4dd698344796
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-13 10:09:50 +0200

    Final final!

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,11 +1,11 @@
-// 1
+// # 1.
 
-var firstName;
+let firstName;
 firstName = "Nadia";
-var lastName = "Daruiz"
+let lastName = "Daruiz"
 console.log (firstName, lastName);
 
-// 2 
+// # 2. 
 
 var name = "John Smith";
 var city = "Berlin";
@@ -18,11 +18,11 @@ console.log (city);
 console.log (profession);
 console.log (hobby);
 
-// 3
+// # 3.
 
 console.log (name + " " + "is a " + age + " " + profession + " " + "who lives in " + city);
 
-// 4
+// # 4.
 
 //let length = 16.78; --> number
 //let lastName = "Johnson"; --> string
@@ -34,17 +34,17 @@ console.log (typeof "Johnson");
 console.log (typeof "");
 console.log (typeof true);
 
-// 5 
+// # 5. 
 
 var ageMark = 20+13;
 var ageJohn = "35";
 console.log (ageMark, ageJohn);
 
-// 6
+// # 6.
 
 console.log ((ageJohn > ageMark) ? "John is older than Mark: true" : "Mark is older than John: false");
 
-// 7 
+// # 7. 
 
 let a;
 a = 2;
@@ -60,20 +60,20 @@ console.log (a === b);
 console.log (a === b);
 console.log (b === c);
 
-// 8
+// # 8.
 
 var programmingLanguage = "JavaScript";
 var isFun = true;
 console.log (programmingLanguage + " is fun: " + isFun);
 
-// 9 
+// # 9. 
 
 var test; 
 console.log (typeof "test");
 
-// 10
+// # 10.
 
-var calculation;
+let calculation;
 calculation = 1+1;
 console.log (calculation);
 
