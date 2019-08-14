a9554f81ff3da550c61a6be1b8c574e28dac2c6a
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-14 12:03:24 +0200

    Ternary Final

---
+++ ternary/index.js
@@ -0,0 +1,41 @@
+// # 1 
+
+const canDrive = 18;
+const cantDrive = 15;
+const ageCheck = (cantDrive >= canDrive) ? "Yes, you can drive! Hope you have a license" 
+: "Nope, you're too young!";
+console.log (ageCheck);
+
+// # 2
+
+const speedLimit = 130;
+const speed = 150;
+const speedCheck = (speed >= speedLimit) ? "You're going too fast - slow down!"
+: "You're driving below the speed limit";
+console.log (speedCheck);
+
+// # 3 
+
+const legalAge = 21;
+const age = 25;
+const ageChecked = (legalAge >= age) ? "Here's some juice" : "Here's some wine";
+console.log(ageChecked);
+
+// # 4
+
+const student = true;
+const isStudent = (student) ? "Ticket cost 5EUR" : "Ticket cost 12EUR";
+console.log (isStudent);
+
+const anotherStudent = "yes";
+const answer =  "no";
+const latestCheck = ("no" === anotherStudent) ? "Ticket cost 5EUR" : "Ticket cost 12EUR"
+console.log(latestCheck);
+
+// # 5
+
+const morning = true;
+const timeOfDay = (morning) ? "Good Morning!" : "Good Evening!";
+console.log(timeOfDay);
+
+
