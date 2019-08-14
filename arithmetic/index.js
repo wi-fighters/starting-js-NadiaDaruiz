1b41d5b541c49f5ad8fadd94694efa568d6c0c93
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-14 09:11:02 +0200

    Add ex 1 to 5

---
+++ arithmetic/index.js
@@ -0,0 +1,73 @@
+// # 1. Percentage
+
+let result = 17 / 30;
+console.log(`${result} %`);
+
+
+// # 2. Avarages
+
+const workedHours = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 7 + 4 + 8.5
+console.log("In the last two weeks she worked " + workedHours);
+const workedAvarage = workedHours / 10; 
+console.log("She worked " + workedAvarage + " per day.");
+
+// ## 3. Score
+
+const scoredTest = 75 + 70 + 85 + 90 + 100;
+console.log(scoredTest);
+const avarage = scoredTest / 5;
+console.log(avarage);
+const finalAvarage = 85;
+const sixthTest = avarage - finalAvarage;
+console.log(sixthTest);
+result = sixthTest
+console.log("Score in the sixth test " + result);
+
+// ## 4. Minimum Score
+
+const currentNumbersExams = 8;
+const currantAvarage = 78;
+const totalPreFinalExam = currentNumbersExams * currantAvarage;
+const totalNumberOfExams = 9;
+const desiredAvarage = 80; 
+const minumumResultNeeded = (desiredAvarage * totalNumberOfExams) - (totalPreFinalExam);
+console.log(`James needs a minimum of ${minumumResultNeeded} to get an 80% average`);
+
+// ## 5. Consumer formula
+
+const checkOne = 22.35;
+const checkTwo = 26.67;
+const checkThree = 35.92;
+const tipPercentageOne = 100 / 10;
+const tipPercentageTwo = 100 / 15;
+const tipPercentageThree = 100 / 20;
+const checkOneWithTip = checkOne + tipPercentageOne;
+const checkTwoWithTip = checkTwo + tipPercentageTwo;
+const checkThreeWithTip = checkThree + tipPercentageThree;
+
+console.log(checkOneWithTip);
+console.log(checkTwoWithTip);
+console.log(checkThreeWithTip);
+
+// ## 6. Geometry Formulas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
