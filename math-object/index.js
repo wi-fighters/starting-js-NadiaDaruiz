53e262c9dbac558b2409c14f31f66e58fdc04b6e
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-19 22:36:10 +0200

    Add ex 1 and 2

--- math-object/index.js
+++ math-object/index.js
@@ -2,5 +2,15 @@
 
 // ##1 Lowest number
 
+console.log (Math.min (4, 6, 93455666, 79, 30006, 12045));
+console.log (Math.min (1.5, 2.9, 99.9, -9, 444, -5.9));
+
+// ## 2 Highest number
+
+console.log (Math.max( 9999, 9.6, 1.8999, 1000, -1.99, 10932));
+
+// ## 3 Rounding
+
+
 
 
