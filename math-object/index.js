1c9d44082c0eb01423972b533c787889e4428498
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-19 23:06:17 +0200

    Add ex 4 and 5

--- math-object/index.js
+++ math-object/index.js
@@ -1,15 +1,40 @@
 // # Math Object
 
-// ##1 Lowest number
+    // ## 1. Lowest number
 
-console.log (Math.min (4, 6, 93455666, 79, 30006, 12045));
-console.log (Math.min (1.5, 2.9, 99.9, -9, 444, -5.9));
+    console.log(Math.min(4, 6, 93455666, 79, 30006, 12045));
+    console.log(Math.min(1.5, 2.9, 99.9, -9, 444, -5.9));
 
-// ## 2 Highest number
+    // ## 2. Highest number
 
-console.log (Math.max( 9999, 9.6, 1.8999, 1000, -1.99, 10932));
+    console.log(Math.max(9999, 9.6, 1.8999, 1000, -1.99, 10932));
 
-// ## 3 Rounding
+    // ## 3. Rounding 
+
+        // ### Up
+
+        console.log(Math.ceil(3321.32321));
+        console.log(Math.ceil(326.76));
+        console.log(Math.ceil (76788.7));
+        console.log(Math.ceil(-9.78));
+        console.log(Math.ceil (43.342));
+
+        // ### Down
+
+        console.log(Math.floor(3321.32321));
+        console.log(Math.floor(326.76));
+        console.log(Math.floor(76788.7));
+        console.log(Math.floor(-9.78));
+        console.log(Math.floor (43.342));
+
+    // ## 4. Dice roll!
+
+    console.log(Math.floor(Math.random()* 6) +1 );
+
+
+
+
+
 
 
 
