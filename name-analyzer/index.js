39141b9be3c8f6e1e3712fe24c083f8a43cef8d9
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-08-19 23:11:45 +0200

    Small spelling error

--- name-analyzer/index.js
+++ name-analyzer/index.js
@@ -4,12 +4,12 @@
 const welcome ="Hello";
 const firstName = "Winston"; 
 const firstLetter = firstName.substring(0,1);
-const lastletter = firstName.substring (6);
+const lastLetter = firstName.substring (6);
 const funnyName = "Winschmoo";
 
 console.log(`${welcome}, ${firstName}. 
 Your name is ${firstName.length} letters long.
-The first letter of your name is ${firstLetter} and the last one is ${lastletter}.
+The first letter of your name is ${firstLetter} and the last one is ${lastLetter}.
 If I want to be funny, I will call you ${funnyName}.`);
 
 // ## 2
@@ -18,10 +18,10 @@ const welcome ="Hello";
 const fullName = " Winston Churchill "; 
 const trimName = fullName.trim (); 
 const firstLetter = trimName.substring(0,1);
-const lastletter = trimName.substring (6,7);
+const lastLetter = trimName.substring (6,7);
 const funnyName = "Winschmoo";
 
 console.log(`${welcome}, ${fullName}. 
 Your name is ${fullName.length} letters long.
-The first letter of your name is ${firstLetter} and the last one is ${lastletter}.
+The first letter of your name is ${firstLetter} and the last one is ${lastLetter}.
 If I want to be funny, I will call you ${funnyName}.`);
